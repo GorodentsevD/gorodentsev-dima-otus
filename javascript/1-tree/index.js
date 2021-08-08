4d44f6907ed0c5bab10e0c69fd723cfc963b2196
dmitry_gorodentcev <dmitry.gorodentcev@elementpay.io>
2021-08-08 15:19:43 +0700

    added tests for tree module

--- javascript/1-tree/index.js
+++ javascript/1-tree/index.js
@@ -1,5 +1,4 @@
-const childLine = '└──';
-const separator = '|   ';
+const tree = require('./tree');
 
 const json = {
     "name": 1,
@@ -12,33 +11,5 @@ const json = {
     }]
 }
 
-
-const printName = (val, tabCount) => {
-    let tabStr;
-    if (tabCount === 0) {
-        tabStr = '';
-    } else if (tabCount === 1) {
-        tabStr = childLine + ' ';
-    } else {
-        tabStr = separator.repeat(tabCount - 1) + childLine + ' ';
-    }
-
-    console.log(tabStr + val);
-}
-
-
-const printTree = (json, tabCount = 0) => {
-    const keys = Object.keys(json);
-    keys.forEach((key, idx) => {
-        if (key === 'name') {
-            printName(json[key], tabCount);
-        }
-
-        if (key === 'items') {
-            json[key].forEach(obj => printTree(obj, tabCount + 1));
-        }
-    });
-};
-
-
-printTree(json);
+tree(json);
+tree({});
