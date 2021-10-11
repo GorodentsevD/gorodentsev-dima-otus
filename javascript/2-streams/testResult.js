4336775049192358c50fdfaae448d63c6e33f8a6
dmitry_gorodentcev <dmitry.gorodentcev@elementpay.io>
2021-10-11 14:40:27 +0700

    #2 added test

---
+++ javascript/2-streams/testResult.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const fs = require('fs');
+
+if (!fs.existsSync('result.txt')) {
+    console.log('Result file `result.txt` does not exist');
+    return;
+}
+
+console.log('Result check started...')
+
+try {
+    fs.readFileSync('result.txt').toString()
+        .split('\n')
+        .map(str => parseInt(str))
+        .reduce((p, c) => {
+            if (p > c) throw new Error('Result is wrong');
+            return c;
+        });
+} catch (e) {
+    console.log(e.message);
+}
+
+console.log('Success');
